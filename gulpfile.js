c76b3b8707acfab728b3f12c939c8d5ca4f6c80d
codefingas <udohezekiel34@yahoo.com>
2022-05-13 18:46:47 +0100

    feature: set up compilation workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
-    .pipe(sass())
+  return src('index.scss') // recieves relative path to the source file
+    .pipe(sass()) // compiles sass file
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['index.scss'], buildStyles) //watches the file and builds it
 }
 
 exports.default = series(buildStyles, watchTask)
